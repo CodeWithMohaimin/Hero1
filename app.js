922a01bafc56e9a4ceea986f6f361904602b7f1e
programmermohaimin <you@example.com>
2022-08-21 10:43:56 +0600

    done

--- app.js
+++ app.js
@@ -2,9 +2,5 @@ let bun = document.getElementById('bun');
 let navLink = document.getElementById('navLink');
 
 bun.addEventListener('click', function () {
-    if (navLink.style.display === 'flex') {
-        navLink.style.display = 'none'
-    } else if (navLink.style.display = 'none') {
-        navLink.style.display = 'flex'
-    }
+    navLink.classList.toggle('active')
 })
